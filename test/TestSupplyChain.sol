4191b33694b97bd00cb4b38954bf8cbd5b92bf49
Romil Jain <romil2707@gmail.com>
2019-07-18 20:51:45 -0700

    Moving Proxy contracts out of Contracts folder and keeping it in Test folder

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
-import "../contracts//Proxy.sol";
+import "./Proxy.sol";
 
 contract TestSupplyChain {
 
